11367bccbf11cd511700bf0c52be46d51ca474b1
bkleiner <b.kleiner@str8labs.com>
2022-12-08 23:57:19 +0100

    drop vite-plugin-static-copy

--- vite.config.ts
+++ vite.config.ts
@@ -4,34 +4,12 @@ import { defineConfig } from "vite";
 
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 import topLevelAwait from "vite-plugin-top-level-await";
 import { ViteRsw } from "vite-plugin-rsw";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    ViteRsw(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "node_modules/kissfft-wasm/lib/kissfft.wasm",
-          dest: ".",
-        },
-        {
-          src: "node_modules/kissfft-wasm/lib/kissfft.wasm",
-          dest: "src/worker",
-        },
-        {
-          src: "node_modules/kissfft-wasm/lib/kissfft.wasm",
-          dest: "assets",
-        },
-      ],
-    }),
-    topLevelAwait(),
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [ViteRsw(), topLevelAwait(), vue(), vueJsx()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
